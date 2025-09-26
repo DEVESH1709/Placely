f21de43672f2471387160550ce6d9c49b93a0793
DEVESH1709 <deveshkesharwani2003@gmail.com>
2025-09-26 19:55:03 +0530

    Change background opacity to transparent in Page component filter overlay

--- app/page.tsx
+++ app/page.tsx
@@ -281,7 +281,7 @@ const Page = () => {
             }`}
           >
             <div
-              className="absolute inset-0 bg-black bg-opacity-50"
+              className="absolute inset-0 bg-transparent"
               onClick={() => setIsFilterOpen(false)}
             ></div>
             <div
